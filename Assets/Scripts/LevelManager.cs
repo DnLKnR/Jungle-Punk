a85aa870005589dc98986d5f8acb6bfdbda474fb
DnLKnR <konia013@umn.edu>
2015-10-04 01:42:50 -0500

    Just messing around with initial formatting of the game

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public GameObject spawnPoint;
+	
+	private PlayerController player;
+	
+	// Use this for initialization
+	void Start () {
+		//Get PlayerController Object that already exists in the scene
+		player = FindObjectOfType<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void RespawnPlayer(){
+		//Turn player off
+		TogglePlayer(false);
+		//Reset the players position to their last checkpoint position
+		player.transform.position = spawnPoint.transform.position;
+		Debug.Log ("Player Respawn");
+		//Turn player back on
+		TogglePlayer(true);
+	}
+	
+	public void TogglePlayer(bool isEnabled){
+		//Get Renderer object from the player
+		Renderer renderer = player.GetComponent<Renderer>();
+		//Get RigidBody2D object from player
+		Rigidbody2D rigidBody2D = player.GetComponent<Rigidbody2D>();
+		//Stop all player movement
+		rigidBody2D.velocity = Vector2.zero;
+		//Enable/Disable player being controlled
+		player.enabled = isEnabled;
+		//Enable/Disable player form being visible
+		renderer.enabled = isEnabled;
+	}
+}
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	[HideInInspector] public Rigidbody2D rigidBody2D;
+	
+	public float moveSpeed;
+	private float moveVelocity;
+	public float jumpHeight;
+	
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
+	
+	// Use this for initialization
+	void Start () {
+		//Get the rigidBody2D object
+		rigidBody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate(){
+		
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+	// Update is called once per frame
+	void Update () {
+		//Input.GetKeyDown means "Key was Pressed", KeyCode.Space == "Spacebar"
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			//Check if the player is touching the ground
+			if (grounded) {
+				Jump();
+			}
+		}
+		//To prevent sliding, make sure to start at velocity 0
+		moveVelocity = 0f;
+		//Input.GetKey means "Key is Pressed", KeyCode.D == "D"
+		if (Input.GetKey(KeyCode.D)) {
+			//set moveVelocity in the positive x-direction
+			moveVelocity = moveSpeed;
+		}
+		//Input.GetKeyDown means "Key was Pressed", KeyCode.A means "A"
+		if (Input.GetKey(KeyCode.A)) {
+			//set moveVelocity in the negative x-direction
+			moveVelocity = -moveSpeed;
+		}
+		//move our player in the x-direction
+		Move(moveVelocity);
+		
+		//If character is moving to the left, keep scale normal
+		if (rigidBody2D.velocity.x > 0) {
+			transform.localScale = new Vector3(1f, 1f, 1f);
+		} 
+		//If character is moving to the left, relect the player
+		else if (rigidBody2D.velocity.x < 0) {
+			transform.localScale = new Vector3(-1f, 1f, 1f);
+		}
+	}
+	/* This function causes the player to jump when called */
+	public void Jump(){
+		float x = rigidBody2D.velocity.x;
+		//Use the Rigidbody2D was created on our player and adjust his velocity in the y-direction
+		rigidBody2D.velocity = new Vector2 (x, jumpHeight);
+	}
+	/* This function causes the player to move in x-direction */
+	public void Move(float speed) {
+		//Get the players current y vector
+		float y = rigidBody2D.velocity.y;
+		//Use the Rigidbody2D that was created on our player, adjust his velocity in the x-direction
+		rigidBody2D.velocity = new Vector2(speed, y);
+	}
+}
